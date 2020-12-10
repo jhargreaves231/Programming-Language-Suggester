73640b3320f84711fb4c2bba0bf4071f5f18257b
Jordan Hargreaves <jhargreaves231@gmail.com>
2020-12-10 14:06:47 -0800

    add another variable to be considered in JavaScript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,36 @@
 $(document).ready(function() {
   $("form#langSelect").submit(function(event){
     const questionTwo = $("select#questionTwo").val();
+    const questionFive = $("input:radio[name=company]:checked").val();
 
-    if (questionTwo === "choiceOne") {
+    if (questionTwo === "choiceOne" && questionFive == "largeCompany") {
       $("#cSharp").show()
       $("#Ruby").hide()
       $("#Javacript").hide()
-    } else if (questionTwo ==="choiceTwo") {
+    } else if (questionTwo === "choiceOne" && questionFive == "Startup") {
+      $("#cSharp").show()
+      $("#Ruby").hide()
+      $("#Javacript").hide()
+    } else if (questionTwo === "choiceOne" && questionFive == "Robot") {
+      $("#cSharp").show()
+      $("#Ruby").hide()
+      $("#Javascript").hide()
+    } else if (questionTwo ==="choiceTwo" && questionFive == "largeCompany") {
       $("#Ruby").show()
       $("#cSharp").hide()
+      $("#Javascript").hide()
+    } else if (questionTwo ==="choiceTwo" && questionFive == "Startup") {
+      $("#Ruby").show()
+      $("#cSharp").hide()
+      $("#Javascript").hide()
+    } else if (questionTwo ==="choiceTwo" && questionFive == "Robot") {
+      $("#Ruby").hide()
+      $("#cSharp").show()
+      $("#Javascript").hide()
+    } else if (questionTwo ==="choiceThree" && questionFive == "Robot") {
+      $("#Ruby").hide()
+      $("#cSharp").show()
+      $("#Javascript").hide()
     } else {
       $("#Javascript").show()
       $("#cSharp").hide()
