00806fdb3d793e07260634a49c0e8146aaae3271
Jordan Hargreaves <jhargreaves231@gmail.com>
2020-12-18 19:30:18 -0800

    update the JavaScript to add inputs by using numbers and give results based off a totalScore if else statement.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,21 @@
 $(document).ready(function() {
   $("form#langSelect").submit(function(event){
-    const questionTwo = $("select#questionTwo").val();
-    const questionFive = $("input:radio[name=company]:checked").val();
+    const Two = $("select#questionTwo").val();
+    const Five = $("input:radio[name=company]:checked").val();
 
-    if (questionTwo === "choiceOne" && questionFive == "largeCompany") {
+
+    const questionTwo = parseInt(Two)
+    const questionFive = parseInt(Five)
+
+    const totalScore = questionTwo + questionFive
+
+    if (totalScore <= 2){
       $("#cSharp").show()
       $("#Ruby").hide()
       $("#Javacript").hide()
-    } else if (questionTwo === "choiceOne" && questionFive == "Startup") {
-      $("#cSharp").show()
-      $("#Ruby").hide()
-      $("#Javacript").hide()
-    } else if (questionTwo === "choiceOne" && questionFive == "Robot") {
-      $("#cSharp").show()
-      $("#Ruby").hide()
-      $("#Javascript").hide()
-    } else if (questionTwo ==="choiceTwo" && questionFive == "largeCompany") {
+    } else if (totalScore <= 4) {
       $("#Ruby").show()
       $("#cSharp").hide()
-      $("#Javascript").hide()
-    } else if (questionTwo ==="choiceTwo" && questionFive == "Startup") {
-      $("#Ruby").show()
-      $("#cSharp").hide()
-      $("#Javascript").hide()
-    } else if (questionTwo ==="choiceTwo" && questionFive == "Robot") {
-      $("#Ruby").hide()
-      $("#cSharp").show()
-      $("#Javascript").hide()
-    } else if (questionTwo ==="choiceThree" && questionFive == "Robot") {
-      $("#Ruby").hide()
-      $("#cSharp").show()
       $("#Javascript").hide()
     } else {
       $("#Javascript").show()
@@ -40,3 +26,43 @@ $(document).ready(function() {
     event.preventDefault();
   });
 });
+
+
+/*
+   if (questionTwo === "1" && questionFive == "1") {
+      $("#cSharp").show()
+      $("#Ruby").hide()
+      $("#Javacript").hide()
+    } else if (questionTwo === "1" && questionFive == "2") {
+      $("#cSharp").show()
+      $("#Ruby").hide()
+      $("#Javacript").hide()
+    } else if (questionTwo === "1" && questionFive == "3") {
+      $("#cSharp").show()
+      $("#Ruby").hide()
+      $("#Javascript").hide()
+    } else if (questionTwo ==="2" && questionFive == "1") {
+      $("#Ruby").show()
+      $("#cSharp").hide()
+      $("#Javascript").hide()
+    } else if (questionTwo ==="2" && questionFive == "2") {
+      $("#Ruby").show()
+      $("#cSharp").hide()
+      $("#Javascript").hide()
+    } else if (questionTwo ==="2" && questionFive == "3") {
+      $("#Ruby").hide()
+      $("#cSharp").show()
+      $("#Javascript").hide()
+    } else if (questionTwo ==="3" && questionFive == "3") {
+      $("#Ruby").hide()
+      $("#cSharp").show()
+      $("#Javascript").hide()
+    } else {
+      $("#Javascript").show()
+      $("#cSharp").hide()
+      $("#Ruby").hide()
+    }
+  
+    event.preventDefault();
+  });
+}); */
